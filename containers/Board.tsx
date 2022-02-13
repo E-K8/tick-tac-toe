738c7bcf99505cec1b2a5e3eae91997d65b5ddbf
E-K8 <ekatplat@gmail.com>
2022-02-13 22:23:35 +0000

    initial file structure

---
+++ containers/Board.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return <div>This is the board</div>;
+}
+
+export default Board;
